a42ef45681f44ad456b23f099a653229250c1820
Eduardo Prado <e_prado@estudiantec.cr>
2021-03-05 17:49:26 -0600

    End Creacion
    Se termina de crear los directorios con un main base

---
+++ Rust/target/debug/Rust.d
@@ -0,0 +1,1 @@
+C:\Users\eduar\Documents\TEC\2021\Analisis_S1\Casos\Caso3\Rust\target\debug\Rust.exe: C:\Users\eduar\Documents\TEC\2021\Analisis_S1\Casos\Caso3\Rust\src\main.rs
---
+++ Rust/target/debug/deps/Rust.d
@@ -0,0 +1,5 @@
+C:\Users\eduar\Documents\TEC\2021\Analisis_S1\Casos\Caso3\Rust\target\debug\deps\Rust.exe: src\main.rs
+
+C:\Users\eduar\Documents\TEC\2021\Analisis_S1\Casos\Caso3\Rust\target\debug\deps\Rust.d: src\main.rs
+
+src\main.rs:
