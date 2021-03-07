4d69d6103870710064d5ff6525380f212c9c045f
RandyFonsec <49202815+RandyFonsec@users.noreply.github.com>
2021-03-06 18:07:12 -0600

    Update main.cpp

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,4 +1,32 @@
-//
-// Created by eduar on 3/4/2021.
-//
+#include <iostream>
 
+
+using namespace std;
+const int SIZE = 100000;
+
+void linearSearch(int pArray[], int pNumber){
+	bool found = false;
+	for(int i = 0; i<SIZE; i++){
+		if(pArray[i] == pNumber){
+			cout<<"El numero buscado esta en la posicion "<< i<<endl;
+			found = true;
+			break;
+		}
+		
+	}
+	if(!found)
+		cout<<"El numero no fue encontrado"<<endl;
+	cout<<"Fin \n";
+}
+
+int main(){
+
+	int array[SIZE];
+	for (int i = 0; i<SIZE; i++){
+		array[i] = i+1;
+	}
+
+  linearSearch(array, SIZE+1);
+
+  return 0;
+}
